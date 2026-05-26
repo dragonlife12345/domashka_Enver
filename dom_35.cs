2fc90d84a5dc55e33adf9045b7b9bc2a6c2de925
dragonlife12345 <dragonbedrock@gmail.com>
2026-05-26 11:01:46 +0300

    Add files via upload

---
+++ dom_35.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Пути к файлам
+        string[] files = {
+            "file1.txt",
+            "file2.txt",
+            "file3.txt"
+        };
+
+        Console.Write("Введите слово для поиска: ");
+        string word = Console.ReadLine();
+
+        bool found = SearchWordInFiles(files, word);
+
+        Console.WriteLine(found);
+    }
+
+    static bool SearchWordInFiles(string[] files, string word)
+    {
+        foreach (string file in files)
+        {
+            if (File.Exists(file))
+            {
+                string content = File.ReadAllText(file);
+
+                if (content.Contains(word))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
