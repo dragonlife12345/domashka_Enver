23bd2f4d810141a82aa808d3da3a514f1adfbe08
dragonlife12345 <dragonbedrock@gmail.com>
2026-05-26 10:54:18 +0300

    Add files via upload

---
+++ dom_14.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string password;
+
+        while (true)
+        {
+            Console.Write("Введите пароль: ");
+            password = Console.ReadLine();
+
+            // Проверка длины
+            bool lengthCheck = password.Length == 5;
+
+            // Подсчет цифр
+            int digitCount = password.Count(char.IsDigit);
+
+            // Подсчет спецсимволов
+            int specialCount = password.Count(ch => !char.IsLetterOrDigit(ch));
+
+            // Проверки
+            bool digitCheck = digitCount >= 3;
+            bool specialCheck = specialCount >= 1;
+
+            if (lengthCheck && digitCheck && specialCheck)
+            {
+                Console.WriteLine("Пароль подходит!");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Пароль не подходит.");
+                Console.WriteLine("Условия:");
+                Console.WriteLine("- Длина 5 символов");
+                Console.WriteLine("- Минимум 3 цифры");
+                Console.WriteLine("- Минимум 1 спецсимвол");
+                Console.WriteLine("Попробуйте снова.\n");
+            }
+        }
+    }
+}
